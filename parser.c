6853d78d444059886313f68b2eec57d9553bc2c7
Odinaka nnamani <Robertpaschal>
2023-10-17 05:51:55 -0700

    parser

--- parser.c
+++ parser.c
@@ -8,14 +8,15 @@
 
 void tokenize_input(char *input, char **args)
 {
-	char *token;
+	char *args[256] = {NULL};
 	int index = 0;
+	char *token;
 
-	token = _strtok(input, " \t");
+	token = _strtok(buf, " \t");
 	while (token != NULL && index < 255)
 	{
 		args[index] = token;
-		token = strtok(NULL, " \t");
+		token = _strtok(NULL, " \t");
 		index++;
 	}
 	args[index] = NULL;
